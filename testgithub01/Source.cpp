751366eff59f53d864146f19498de102a4233433
PRACHARAPOL JARUVANAWAT <63010630@KMITL.AC.TH>
2020-08-10 11:31:45 +0700

    success

---
+++ testgithub01/Source.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int plus() {
+	printf("%f\n", 15.2 + 16.8);
+}
+int minus() {
+	printf("%d\n", 20 - 12);
+}
+int main() {
+	printf("\nhello world");
+	plus();
+	minus();
+	return 0;
+}
